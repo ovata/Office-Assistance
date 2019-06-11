a6e6baa5d2453e75292b3440f0c87412f74293dd
Victor Atoyebi <tatoyebi@eminenttechnology.com>
2019-06-11 15:06:32 +0100

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgxSmartModalModule } from 'ngx-smart-modal';
+
 import { AppComponent } from './app.component';
+import { DBodyComponent } from './components/dbody/dbody.component';
+import { DFooterComponent } from './components/dfooter/dfooter.component';
+import { DHeaderComponent } from './components/dheader/dheader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DBodyComponent,
+    DFooterComponent,
+    DHeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxSmartModalModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
